e3838823cd67167050bc5d0bace845995452e4e7
Arphie <arphie@Arphies-Mac-mini.local>
2024-10-10 20:46:19 +0800

    initial progress

---
+++ global/menu.php
@@ -0,0 +1,21 @@
+<div class="d-ah-logo">
+    <img src="/assets/images/logo.png" alt="">
+</div>
+<div class="d-ah-menu">
+    <div class="d-main-menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">FAQs</a></li>
+        </ul>
+    </div>
+    <div class="d-main-options">
+        <ul>
+            <li><a href="#">Request an Account</a></li>
+            <li><a href="#">Log in</a></li>
+        </ul>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <link rel="stylesheet" href="/assets/css/fonts.css">
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="/assets/css/homepage.css">
+
+        <script src="https://kit.fontawesome.com/28b5b7d92f.js" crossorigin="anonymous"></script>
+
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="d-main-container">
+            <div class="d-main-inner-container">
+                <div class="d-header-part">
+                    <div class="d-header-container">
+                        <div class="d-head-top">
+                            <?php include 'global/menu.php'; ?>
+                        </div>
+                        <div class="d-head-middle">
+                            <div class="d-ah-into">Discovery & Motion Work Support for Plaintiffs Personal Injury Law Firms</div>
+                            <div class="d-ah-sub-into">Pay-as-you-go, 24/7 team so you can handle your discovery & motion work needs with confidence.</div>
+                            <div class="d-ah-head-options">
+                                <ul>
+                                    <li><a href="#">Start for free</a></li>
+                                    <li><a href="#">See Demo</a></li>
+                                </ul>
+                            </div>
+                            <div class="d-phone-segment">
+                                <div class="d-phone-db">
+                                    <ul>
+                                        <li><img src="/assets/images/home_notif_1.png" alt=""></li>
+                                        <li><img src="/assets/images/home_notif_2.png" alt=""></li>
+                                        <li><img src="/assets/images/home_notif_3.png" alt=""></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="d-under-phone">
+                                <h5>Never get overwhelmed...</h5>
+                                <ul>
+                                    <li>By scrambling to cover for staff on emergency leave,</li>
+                                    <li>By key talents leaving your firm at the most unexpected times,</li>
+                                    <li>By increased caseloads and last-minute deadlines.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-handle-case-part">
+                    <div class="d-handle-inner">
+                        <div class="d-handle-left">
+                            <img src="/assets/images/ah_female_model.png" alt="">
+                        </div>
+                        <div class="d-handle-right">
+                            <h5>Handle your cases with confidence...</h5>
+                            <ul>
+                                <li><strong>24/7</strong> on-demand team.</li>
+                                <li>Specialized in <strong>Discovery & Motion Work</strong> support for Plaintiffs Personal Injury law firms</li>
+                                <li><strong>Collaborative Platform</strong> & Easy Task Delegation </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-handle-understand">
+                    <img src="/assets/images/handle_understand.jpg" alt="">
+                </div>
+                <div class="d-handle-review"></div>
+                <div class="d-handle-pricing">
+                    <div class="d-handle-priing-inner">
+                        <h5>Pricing</h5>
+                        <div class="d-pricing-info">
+                            <div class="d-pricing-info-left">
+                                <div class="d-pricing-base">
+                                    <div class="d-avocado-image">
+                                        <img src="/assets/images/avocado-image.png" alt="">
+                                    </div>
+                                    <div class="d-avocado-separator">&nbsp;</div>
+                                    <div class="d-avocado-price">$20/hr</div>
+                                </div>
+                            </div>
+                            <div class="d-pricing-info-right">
+                                <div class="d-pricing-right-inner">
+                                    <h5>No-Expiry Credits!</h5>
+                                    <ul>
+                                        <li>Prepaid</li>
+                                        <li>No minimum purchase required</li>
+                                        <li>Bill only for Productive Time</li>
+                                        <li>24/7 Operations</li>
+                                        <li>Serviced by Philippine-Bar licensed attorneys trained with US litigation work</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-learn-more">
+                            <a href="#">Learn more about our pricing</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-handle-get-started">
+                    <div class="d-handle-started">
+                        <h5>How to get started?</h5>
+                        <div class="d-handle-numbers">
+                            <div class="d-numbers">
+                                <div class="d-number-item number-one"><span><i class="fa-solid fa-1"></i></span></div>
+                                <div class="d-number-item number-two"><span><i class="fa-solid fa-2"></i></span></div>
+                                <div class="d-number-item number-three"><span><i class="fa-solid fa-3"></i></span></div>
+                            </div>
+                            <div class="d-dots">&nbsp;</div>
+                        </div>
+                        <div class="d-handle-info">
+                            <div class="d-handle-base dh-item-one">
+                                <div class="d-handle-desc-intro">Request an<br />account</div>
+                                <div class="d-handle-desc-desc">By requesting an account, you can set your preferences ahead so you get assigned a “Hive” that fits your needs.</div>
+                            </div>
+                            <div class="d-handle-base dh-item-two">
+                                <div class="d-handle-desc-intro">Delegate<br />Tasks</div>
+                                <div class="d-handle-desc-desc">Once your account is active, start assigning tasks to your assigned “Hive”.</div>
+                            </div>
+                            <div class="d-handle-base dh-item-three">
+                                <div class="d-handle-desc-intro">Optimize &<br />Collaborate</div>
+                                <div class="d-handle-desc-desc">After assigning tasks, Avocado Hive’s transition team and your assigned “Hive” will work with you to optimize your delegation style, ensuring your firm is 24/7 service ready.</div>
+                            </div>
+                        </div>
+                        <div class="d-learn-more">
+                            <a href="#">Learn more about our services</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-handle-get-footer">
+                    <div class="d-handle-footer-inner">
+                        <div class="d-cols-item cols-one">
+                            <div class="d-foot-title">About Us</div>
+                            <div class="d-foot-desc">Avocado Hive Inc. is a domestic corporation registered in the Philippines that specializes in working with solos and small law firm Attorneys in North America & Australia</div>
+                            <div class="d-foot-social">
+                                <ul>
+                                    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="d-cols-item cols-two">
+                            <div class="d-foot-title">Contact Us</div>
+                            <div class="d-foot-contact">
+                                <ul>
+                                    <li><i class="fa-solid fa-envelope"></i> <span><a href="mailto:info@avocadohive.com">info@avocadohive.com</a></span></li>
+                                    <li><i class="fa-regular fa-clock"></i> <span>Services Hours : 24/7</span></li>
+                                    <li><i class="fa-solid fa-location-dot"></i> <span>Ajeno Building, Corner<br />Andrade & Yumang Street,<br />General Santos City</span></li>
+                                    <li><i class="fa-solid fa-phone"></i> <span>+1 617 752 3743  US</span></li>
+                                    <li><i class="fa-regular fa-calendar-days"></i> <span>Corporate Office hours<br />9am to 5pm eastern time</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="/assets/css/main.js"></script>
+        <script src="/assets/css/homepage.js"></script>
+    </body>
+</html>
